2a4ce40fc413a20ccea3c5bd7a3d7da32b74b318
Sittikorn Hirunpongthawat <buskun.sittikorn@gmail.com>
2019-05-06 14:37:30 +0700

    Add feature : CustomCursor

--- includes/SDL_util.h
+++ includes/SDL_util.h
@@ -11,29 +11,43 @@
 #include "util/array.h"
 
 namespace SDL {
+	extern bool isCustomCursor;
+	extern SDL_Cursor *systemCursor;
+	extern SDL_Cursor *customCursor;
+
 	void logSDLError(const std::string &message, bool exitOnError);
 
-	SDL_Texture * loadTexture(SDL_Renderer *SDLRenderer, const std::string &texturePath);
+	SDL_Texture *loadTexture(SDL_Renderer *SDLRenderer, const std::string &texturePath);
 
 	void renderTexture(SDL_Renderer *SDLRenderer, SDL_Texture *texture, int posX, int posY);
 
 	void renderTexture(SDL_Renderer *SDLRenderer, SDL_Texture *texture, int posX, int posY, int width, int height);
+
+	void setSystemCursor(SDL_SystemCursor systemCursorID);
+
+	void setCustomCursor(const std::string &surfacePath, int hot_x, int hot_y);
+
+	void toggleCursor();
+
+	void useCustomCursor();
+
+	void useSystemCursor();
 };
 
 class EventManager {
-	SDL_Event event = { };
+	SDL_Event event = {};
 	bool exitProgram = true;
 	Timer *timer = nullptr;
 	std::map<SDL_EventType, Array<std::function<void(SDL_Event)>> *> callbackList;
 	std::map<SDL_EventType, Array<std::function<void(SDL_Event)>> *> gameTickCallbackList;
 public:
-	EventManager( );
+	EventManager();
 
 	EventManager *init(std::function<bool(SDL_Event)> &&callback);
 
-	void exit( );
+	void exit();
 
-	void gameTick( );
+	void gameTick();
 
 	EventManager *on(SDL_EventType eventType, std::function<void(SDL_Event)> &&callback);
 
--- scenes/menu.cpp
+++ scenes/menu.cpp
@@ -1,24 +1,28 @@
 #include "scene_list.h"
 
 void menu(GameScenes *gameScenes) {
-    Scene *scene = gameScenes->newScene("menu");
-    Container *sceneContainer = scene->getSceneContainer();
+	Scene *scene = gameScenes->newScene("menu");
+	Container *sceneContainer = scene->getSceneContainer();
 
-    RendererController *rendererController = gameScenes->getRendererController();
-    EventManager *eventManager = gameScenes->getEventManager();
+	RendererController *rendererController = gameScenes->getRendererController();
+	EventManager *eventManager = gameScenes->getEventManager();
 
-    GameProp gameProp = gameScenes->getGameProp();
+	GameProp gameProp = gameScenes->getGameProp();
 
-    auto text = new TextView(rendererController,
-                             "Hello world!",
-                             {24, gameProp.RESOURCE_PATH + "/fonts/Roboto-Regular.ttf", {255, 255, 255}});
-    text->setHoverText("Hella world!", {24, gameProp.RESOURCE_PATH + "/fonts/Roboto-Regular.ttf", {255, 255, 255}});
-    sceneContainer->append(text);
+	auto text = new TextView(rendererController,
+	                         "Hello world!",
+	                         {24, gameProp.RESOURCE_PATH + "/fonts/Roboto-Regular.ttf", {255, 255, 255}});
+	text->setHoverText("Hella world!", {24, gameProp.RESOURCE_PATH + "/fonts/Roboto-Regular.ttf", {255, 255, 255}});
+	sceneContainer->append(text);
 
-    eventManager->on(SDL_MOUSEBUTTONDOWN, [=](SDL_Event event) {
-        text->show(!text->isShown());
-    });
+	SDL::setCustomCursor(gameProp.RESOURCE_PATH + "/img/image.bmp", 0, 0);
+	SDL::setSystemCursor(SDL_SYSTEM_CURSOR_ARROW);
 
-    scene->onGameTick([](Scene *currentScene) {
-    });
+	eventManager->on(SDL_MOUSEBUTTONDOWN, [=](SDL_Event event) {
+		text->show(!text->isShown());
+		SDL::toggleCursor();
+	});
+
+	scene->onGameTick([](Scene *currentScene) {
+	});
 }
